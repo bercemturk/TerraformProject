0df48a77ef0839e65fda2d58ac80c24358f84061
Omurbek Orozaliev <99226984+Oma1993@users.noreply.github.com>
2022-07-30 12:12:47 -0500

    Update

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.Team2.name
   virtual_network_name = azurerm_virtual_network.Team2.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_linux_virtual_machine_scale_set" "Team2" {
