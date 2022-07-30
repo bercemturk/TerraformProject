33575149b84de2c3d20b2d71f055f50d83f2f161
“Oma1993” <noemonstrov@gmail,com>
2022-07-30 02:13:57 +0000

    updated

--- scaleset.tf
+++ scaleset.tf
@@ -2,31 +2,32 @@ locals {
   first_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtjrl6evUosX8dgR99iGNa18vHHwhvKb7p6ap6LWh1lXfWVLEwi092zdlViDwpaAgDEcCn5cUkSYIjSKkBpA3JbNf8hcBWt4LygSqfT1Oab3bAzZCDS8pJomHc4LfF8PHZQEUmLJ7FZ4AT3+t6kIcAnvzJ92G9LWimK53iSIPboXnKHMoVxOZV6g5i+UpAUPdPVDSNI6gqVRkGmv9FV+7Wd97FeerdIJYgoZh1ByRWBENQKowqtcXctH+9FVSLKoFI1nfl6hK9JQTiIO8fT83lZHDKHLwdh1bDy5YJ1K9JfFqFLM9IAk4T+5IO3SGrn/J7NiVqcOgk8q0K20WoMteV omar@cc-f6bdb78c-7f646f9788-m6mzx"
 }
 
-# resource "azurerm_resource_group" "Team2" {
-#   name     = "my-resources"
-#   location = "East US"
-# }
+resource "azurerm_resource_group" "Team2" {
+  name     = "my-resources"
+  location = "East US"
+}
 
-# resource "azurerm_virtual_network" "Team2" {
-#   name                = "Team2-network"
-#   resource_group_name = azurerm_resource_group.Team2.name
-#   location            = azurerm_resource_group.Team2.location
-#   address_space       = ["10.0.0.0/16"]
-#}
+resource "azurerm_virtual_network" "Team2" {
+  name                = "Team2-network"
+  resource_group_name = azurerm_resource_group.Team2.name
+  location            = azurerm_resource_group.Team2.location
+  address_space       = ["10.0.0.0/16"]
+}
 
-# resource "azurerm_subnet" "internal" {
-#   name                 = "internal"
-#   resource_group_name  = azurerm_resource_group.Team2.name
-#   virtual_network_name = azurerm_virtual_network.Team2.name
-#   address_prefixes     = ["10.0.2.0/24"]
-#}
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.Team2.name
+  virtual_network_name = azurerm_virtual_network.Team2.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
 
 resource "azurerm_linux_virtual_machine_scale_set" "Team2" {
   name                = "Team2-vmss"
   resource_group_name = azurerm_resource_group.Team2.name
   location            = azurerm_resource_group.Team2.location
   sku                 = "Standard_F2"
-  instances           = 1
+  instances           = 3           #you can put only up to 5 
   admin_username      = "adminuser"
 
   admin_ssh_key {
@@ -35,9 +36,9 @@ resource "azurerm_linux_virtual_machine_scale_set" "Team2" {
   }
 
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    publisher = "OpenLogic"
+    offer     = "CentOS"
+    sku       = "7.5"
     version   = "latest"
   }
 
@@ -45,6 +46,8 @@ resource "azurerm_linux_virtual_machine_scale_set" "Team2" {
     storage_account_type = "Standard_LRS"
     caching              = "ReadWrite"
   }
+
+
 
   network_interface {
     name    = "Team2"
@@ -55,6 +58,4 @@ resource "azurerm_linux_virtual_machine_scale_set" "Team2" {
       primary   = true
       subnet_id = azurerm_subnet.internal.id
     }
-  }
-}
-
+  
