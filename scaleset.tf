ba02bdaef5b958590c34a1bebec512ab955f2f1d
Omurbek Orozaliev <99226984+Oma1993@users.noreply.github.com>
2022-07-29 21:25:21 -0500

    local changed

--- scaleset.tf
+++ scaleset.tf
@@ -1,5 +1,5 @@
 locals {
-  first_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtjrl6evUosX8dgR99iGNa18vHHwhvKb7p6ap6LWh1lXfWVLEwi092zdlViDwpaAgDEcCn5cUkSYIjSKkBpA3JbNf8hcBWt4LygSqfT1Oab3bAzZCDS8pJomHc4LfF8PHZQEUmLJ7FZ4AT3+t6kIcAnvzJ92G9LWimK53iSIPboXnKHMoVxOZV6g5i+UpAUPdPVDSNI6gqVRkGmv9FV+7Wd97FeerdIJYgoZh1ByRWBENQKowqtcXctH+9FVSLKoFI1nfl6hK9JQTiIO8fT83lZHDKHLwdh1bDy5YJ1K9JfFqFLM9IAk4T+5IO3SGrn/J7NiVqcOgk8q0K20WoMteV omar@cc-f6bdb78c-7f646f9788-m6mzx"
+  first_public_key = file("~/.ssh/id_rsa.pub")
 }
 
 resource "azurerm_resource_group" "Team2" {
